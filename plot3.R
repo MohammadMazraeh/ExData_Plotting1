11e3baaef7d1b932b4b419f693ea9b7e0652c685
MohammadMazraeh <Mazraeh.Mohammad@gmail.com>
2015-08-09 12:29:47 +0430

    Legen Bug Fixed

--- plot3.R
+++ plot3.R
@@ -5,4 +5,5 @@ with(x, plot(x = Time, y = Sub_metering_1, type = 'n', ylab = 'Energy sub meteri
 with(x, lines(x = Time, y = Sub_metering_1, col = 'black'))
 with(x, lines(x = Time, y = Sub_metering_2, col = 'red'))
 with(x, lines(x = Time, y = Sub_metering_3, col = 'blue'))
+legend('topright', legend = c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), col = c('black','red','blue') , lty = 1)
 dev.off()
